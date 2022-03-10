733ad92c0f621305b3ecc4d1554c772b4b914dcd
Athfan <aththaar47@gmail.com>
2022-03-10 08:33:43 +0530

    Athfan

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome  <a href="https://nextjs.org">Faiza!</a>
+          Welcome  <a href="https://nextjs.org">Athfan!</a>
         </h1>
 
         <p className={styles.description}>
